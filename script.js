fb3a4de34031e38a07596d7d54e39afbf0aea340
Bidhan Rakshit <90621019+Bidhan77@users.noreply.github.com>
2023-08-01 21:07:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-// complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// Set up inheritance: Link Employee's prototype to a new object with Person's prototype as the prototype
+Employee.prototype = Object.create(Person.prototype);
+
+Employee.prototype.jobGreet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+
+// Reset the constructor to Employee
+Employee.prototype.constructor = Employee;
 
 // Do not change code below this line
 window.Person = Person;
